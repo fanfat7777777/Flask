041b2e1ce09fdde7820b0c752522713daddce8e1
fanfat <fanfat@fanfat.ru>
2023-02-28 19:38:58 +0300

    Lesson_02
    То, что делал преподаватель (у меня страницы выдают 404)

--- project/blog/app.py
+++ project/blog/app.py
@@ -1,12 +1,29 @@
 from flask import Flask, request
+from blog.report.views import report
+from blog.user.views import user
 import config
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    if request.method == 'GET':
-        config.count += 1
+#@app.route('/', methods=['GET'])
+#def index():
+#    if request.method == 'GET':
+#        config.count += 1
         
-    return f'Посещение {config.count}'
+#    return f'Посещение {config.count}'
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.run(
+        host='127.0.0.1',
+        debug=True,
+    )
+    register_blueprint(app)
+    return app
+
+
+def register_blueprint(app: Flask):
+    app:register_blueprint(user)
+    app:register_blueprint(report)

2fb76aa619422054bd4736963155f00c500d7e7d
fanfat <fanfat@fanfat.ru>
2023-02-28 15:26:45 +0300

    Переделка первого ДЗ

--- project/blog/app.py
+++ project/blog/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+import config
 
 app = Flask(__name__)
 
-count = 0
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    global count
-    count += 1
-    return f'{count}'
+    if request.method == 'GET':
+        config.count += 1
+        
+    return f'Посещение {config.count}'

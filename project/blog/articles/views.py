6b0190b87187e1a9770806c5bc82443c259bb1a0
fanfat <fanfat@fanfat.ru>
2023-03-01 11:08:41 +0300

    Добавление для страницы статей
    Название, текст, автор

--- project/blog/articles/views.py
+++ project/blog/articles/views.py
@@ -1,13 +1,30 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template
+from werkzeug.exceptions import NotFound
+from blog.user.views import USERS
 
 article = Blueprint('article', __name__, url_prefix='/articles', static_folder='../static')
 
 ARTICLES = {
-    1: 'Статья 1',
-    2: 'Статья 2',
-    3: 'Статья 3',
-    4: 'Статья 4',
-    5: 'Статья 5',
+    1: {
+        'name': 'Статья 1',
+        'text': 'text',
+        'author': 1,
+        },
+    2: {
+        'name': 'Статья 2',
+        'text': 'text',
+        'author': 2,
+        },
+    3: {
+        'name': 'Статья 3',
+        'text': 'text',
+        'author': 3,
+        },
+    4: {
+        'name': 'Статья 4',
+        'text': 'text',
+        'author': 1,
+        },
 }
 
 @article.route('/')
@@ -27,4 +44,5 @@ def get_article(pk: int):
     return render_template(
         'articles/details.html',
         article_name=article_name,
+        article_author=USERS[article_name['author']],
     )

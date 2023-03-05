18974052fa969ce45364d013337d40af09cb3fd9
fanfat7777777 <93639218+fanfat7777777@users.noreply.github.com>
2023-03-05 19:50:59 +0300

    merge Lesson_03 => master
    * Lesson_03
    
    Не сделана аутентификация
    
    * Переделка урока
    
    ---------
    
    Co-authored-by: fanfat <fanfat@fanfat.ru>

--- project/blog/user/views.py
+++ project/blog/user/views.py
@@ -1,33 +1,42 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template
+from flask_login import login_required
 from werkzeug.exceptions import NotFound
-import config
 
-user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
-
-#USERS = ['Alice', 'Jon', 'Mike']
+user = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
 USERS = {
-    1: 'Alice',
-    2: 'Jon',
-    3: 'Mike',
+    1: {"name": "Ivan"},
+    2: {"name": "Jon"},
+    3: {"name": "Mary"}
 }
 
-@user.route('/')
-def user_list():
-    config.count += 1
-    return render_template(
-        'users/list.html',
-        users=USERS,
-        count=config.count,
-        )
 
-@user.route('/<int:pk>')
-def get_user(pk: int):
-    try:
-        user_name = USERS[pk]
-    except KeyError:
-        #raise NotFound(f'User id {pk} not found')
-        return redirect('/users/')
+@user.route("/")
+@login_required
+def user_list():
+    from ..models import User
+    users = User.query.all()
     return render_template(
-        'users/details.html',
-        user_name=user_name,
-        )
+        "users/list.html",
+        users=users
+    )
+
+
+@user.route("/<int:pk>")
+@login_required
+def profile(pk: int):
+    from ..models import User
+    _user = User.query.filter_by(id=pk).one_or_none()
+    if _user is None:
+        raise NotFound("User id:{}, not found".format(pk))
+    return render_template(
+        "users/details.html",
+        user=_user
+    )
+
+
+def get_user_name(pk: int):
+    if pk in USERS:
+        user_name = USERS[pk]["name"]
+    else:
+        raise NotFound("User id:{}, not found".format(pk))
+    return user_name
--- project/wsgi.py
+++ project/wsgi.py
@@ -1,9 +1,18 @@
-from blog.app import create_app
-
-#if __name__ == '__main__':
-#    app.run(
-#        host='0.0.0.0',
-#        debug=True,
-#    )
+from werkzeug.security import generate_password_hash
+from blog.app import create_app, db
 
 app = create_app()
+
+
+@app.cli.command("init-db", help="create all db")
+def init_db():
+    db.create_all()
+
+
+@app.cli.command("create-users", help="create users")
+def create_users():
+    from blog.models import User
+    db.session.add(
+        User(email="name@email.com", password=generate_password_hash("test"))
+    )
+    db.session.commit()

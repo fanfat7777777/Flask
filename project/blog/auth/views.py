835f1260971929fcd1c8538857a1ae200a7b7442
fanfat <fanfat@fanfat.ru>
2023-03-09 10:13:43 +0300

    Lesson_05

--- project/blog/auth/views.py
+++ project/blog/auth/views.py
@@ -1,31 +1,39 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from flask_login import logout_user, login_user, login_required
+from flask_login import logout_user, login_user, login_required, current_user
 from werkzeug.security import check_password_hash
 
-auth = Blueprint("auth", __name__, url_prefix="/auth", static_folder="../static")
+from blog.models import User
+
+auth = Blueprint('auth', __name__, static_folder='../static')
 
 
-@auth.route("/login", methods=["GET", "POST"])
+@auth.route('/login', methods=('GET',))
 def login():
-    if request.method == "GET":
-        return render_template(
-            "auth/login.html"
-        )
-    from ..models import User
+    if current_user.is_authenticated:
+        return redirect(url_for('user.profile', pk=current_user.id))
 
-    email = request.form.get("email")
-    password = request.form.get("password")
+    return render_template(
+        'auth/login.html',
+    )
+
+
+@auth.route('/login', methods=('POST',))
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
     user = User.query.filter_by(email=email).first()
 
     if not user or not check_password_hash(user.password, password):
-        flash("Check your login details")
-        return redirect(url_for(".login"))
+        flash('Check your login details')
+        return redirect(url_for('.login'))
+
     login_user(user)
-    return redirect(url_for("user.profile", pk=user.id))
+    return redirect(url_for('user.profile', pk=user.id))
 
 
-@auth.route("/logout")
+@auth.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for(".login"))
+    return redirect(url_for('.login'))

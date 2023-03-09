9d15f66e8ffea56f5d97be156d6762d7f9257ae2
fanfat7777777 <93639218+fanfat7777777@users.noreply.github.com>
2023-03-09 10:15:50 +0300

    merge Lesson_05 => master
    Co-authored-by: fanfat <fanfat@fanfat.ru>

--- project/blog/app.py
+++ project/blog/app.py
@@ -1,37 +1,24 @@
 from flask import Flask
-from .extension import db, login_manager
-from .article.views import article
-from .models import User
-from .user.views import user
-from .index.views import index
-from .report.views import report
-from .auth.views import auth
 
-
-VIEWS = [
-    index,
-    user,
-    article,
-    report,
-    auth
-]
+from blog import commands
+from blog.extensions import db, login_manager, migrate, csrf
+from blog.models import User
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = '@ag=ii4#ek+^yb4_m8+z=fzjq^)bg#s@yso3vs6u*z603=t_70'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-
+    app.config.from_object('blog.config')
 
     register_extensions(app)
     register_blueprints(app)
+    register_commands(app)
     return app
 
 
 def register_extensions(app):
     db.init_app(app)
+    migrate.init_app(app, db, compare_type=True)
+    csrf.init_app(app)
 
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -42,5 +29,12 @@ def register_extensions(app):
 
 
 def register_blueprints(app: Flask):
-    for view in VIEWS:
-        app.register_blueprint(view)
+    from blog.auth.views import auth
+    from blog.user.views import user
+
+    app.register_blueprint(user)
+    app.register_blueprint(auth)
+
+
+def register_commands(app: Flask):
+    app.cli.add_command(commands.create_init_user)
--- project/blog/user/views.py
+++ project/blog/user/views.py
@@ -1,42 +1,63 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, redirect, url_for, request
+from flask_login import login_required, current_user, login_user
 from werkzeug.exceptions import NotFound
+from werkzeug.security import generate_password_hash
 
-user = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
-USERS = {
-    1: {"name": "Ivan"},
-    2: {"name": "Jon"},
-    3: {"name": "Mary"}
-}
+from blog.extensions import db
+from blog.forms.user import UserRegisterForm
+from blog.models import User
+
+user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 
 
-@user.route("/")
-@login_required
+@user.route('register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('user.profile', pk=current_user.id))
+
+    form = UserRegisterForm(request.form)
+    errors = []
+    if request.method == 'POST' and form.validate_on_submit():
+        if User.query.filter_by(email=form.email.data).count():
+            form.email.errors.append('email already exists')
+            return render_template('users/register.html', form=form)
+
+        _user = User(
+            email=form.email.data,
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            password=generate_password_hash(form.password.data),
+        )
+
+        db.session.add(_user)
+        db.session.commit()
+
+        login_user(_user)
+
+    return render_template(
+        'users/register.html',
+        form=form,
+        errors=errors,
+    )
+
+
+@user.route('/')
 def user_list():
-    from ..models import User
     users = User.query.all()
     return render_template(
-        "users/list.html",
-        users=users
+        'users/list.html',
+        users=users,
     )
 
 
-@user.route("/<int:pk>")
+@user.route('/<int:pk>')
 @login_required
 def profile(pk: int):
-    from ..models import User
-    _user = User.query.filter_by(id=pk).one_or_none()
-    if _user is None:
-        raise NotFound("User id:{}, not found".format(pk))
+    selected_user = User.query.filter_by(id=pk).one_or_none()
+    if not selected_user:
+        raise NotFound(f"User #{pk} doesn't exist!")
+
     return render_template(
-        "users/details.html",
-        user=_user
+        'users/profile.html',
+        user=selected_user,
     )
-
-
-def get_user_name(pk: int):
-    if pk in USERS:
-        user_name = USERS[pk]["name"]
-    else:
-        raise NotFound("User id:{}, not found".format(pk))
-    return user_name
